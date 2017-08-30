6c47fcb0c88ba8ee7725d0deecfd2c745d206d07
Siarhei Slizh <Siarhei_Slizh@epam.com>
2017-08-30 10:28:25 +0300

    Add project files.

---
+++ LocalStorage/IStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalStorage
+{
+    public interface IStorage 
+    {
+        void Add(PortfolioItemDAL item);
+        void Update(PortfolioItemDAL item);
+        void Delete(int id);
+        IEnumerable<PortfolioItemDAL> GetAll();
+        PortfolioItemDAL GetById(int id);
+    }
+}
---
+++ LocalStorage/PortfolioItemDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalStorage
+{
+    public class PortfolioItemDAL
+    {
+        public int ItemId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string Symbol { get; set; }
+
+        public int SharesNumber { get; set; }
+    }
+}
