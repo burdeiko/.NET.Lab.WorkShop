46c60a9dbc57cea6a984f77e070fbb744cd36fcf
Tsimafei Burdzeika <Tsimafei_Burdzeika@epam.com>
2017-08-30 10:54:39 +0300

    Add Mapper

---
+++ PortfolioManagerClient/Infrastructure/Mapper.cs
@@ -0,0 +1,31 @@
+﻿using LocalStorage;
+using PortfolioManagerClient.Models;
+using System;
+
+namespace PortfolioManagerClient.Infrastructure
+{
+    public static class Mapper
+    {
+        public static PortfolioItemViewModel ToViewModel(this PortfolioItemDAL dalItem)
+        {
+            return new PortfolioItemViewModel()
+            {
+                ItemId = dalItem.ItemId,
+                UserId = dalItem.UserId,
+                SharesNumber = dalItem.SharesNumber,
+                Symbol = dalItem.Symbol
+            };
+        }
+
+        public static PortfolioItemDAL ToDALModel(this PortfolioItemViewModel vm)
+        {
+            return new PortfolioItemDAL()
+            {
+                ItemId = vm.ItemId,
+                UserId = vm.UserId,
+                SharesNumber = vm.SharesNumber,
+                Symbol = vm.Symbol
+            };
+        }
+    }
+}
